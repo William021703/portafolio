0ddf37cfb5a79b85d49e0f71bd47609057135b40
William Morillo <morillow02@gmail.com>
2025-06-20 06:58:21 -0400

    estructuras lista

--- src/App.jsx
+++ src/App.jsx
@@ -5,11 +5,6 @@ import "./index.css";
 
 function App() {
   const [name, setName] = useState("");
-  const [skills, setSkills] = useState({
-    languages: ["JavaScript"],
-    frameworks: ["React.js", "Node.js"],
-    databases: ["MySQL"],
-  });
 
   useEffect(() => {
     const nombre = `I'm WiLliam MoRiLlO`;
@@ -43,30 +38,74 @@ function App() {
             English for remote or international work
           </h2>
         </div>
-        <div className="w-full h-screen">
-          <h1>SkiLls</h1>
-          <pre>
-            <code>
-              {Object.entries(skills).map(([key, value], index) => (
-                <div key={index}>
-                  <span className="text-4xl">{key}</span> :{" "}
-                  <span className="text-4xl caret-amber-400">{value}</span>
-                </div>
-              ))}{" "}
-            </code>
-          </pre>
-        </div>
+
         <div>
           <span>
             <TbArrowBigDownLinesFilled className="bg-amber-50 w-60 h-60 animate-bounce text-slate-400" />
           </span>
         </div>
         <div>
-          <h1>ProYectS</h1>
+          <div className="min-h-52 bg-sky-500 min-w-72">
+            <h1>
+              <span>"Languages"</span>: <span>["JavaScript"]</span>
+              <br />
+            </h1>
+            <h1>
+              <span>"Frameworks"</span>: <span>["React", "Node"]</span>
+              <br />
+            </h1>
+            <h1>
+              <span>"Databases"</span>: <span>["MySQL"]</span>
+              <br />
+            </h1>
+          </div>
+
           <div>
-            <img src="/prestamos.png" alt="prestamos" />
-            <img src="/tienda.png" alt="tienda" />
-            <img src="/todolist.png" alt="todolist" />
+            <div>
+              <img src="/prestamos.png" alt="prestamos" />
+              <img src="/tienda.png" alt="tienda" />
+              <img src="/todolist.png" alt="todolist" />
+            </div>
+          </div>
+          <div>
+            <div>
+              <h1>Contacts</h1>
+            </div>
+            <div>
+              <h1>morillow052@gmail.com</h1>
+            </div>
+            <div>
+              <div>
+                <h1>
+                  <a
+                    href="https://www.github.com/william021703/"
+                    target="_blank"
+                  >
+                    <span>Github</span>
+                  </a>
+                </h1>
+              </div>
+              <div>
+                <h1>
+                  <a
+                    href="https://www.linkedin.com/in/william-morillo-4a483a270"
+                    target="_blank"
+                  >
+                    <span>linkedin</span>
+                  </a>
+                </h1>
+              </div>
+              <div>
+                <h1>
+                  <a
+                    href="/PORTAFOLIO/miPortafolio/public/William_cv_updated.pdf"
+                    download
+                  >
+                    <span> download CV</span>
+                  </a>
+                </h1>
+              </div>
+            </div>
           </div>
         </div>
       </div>
